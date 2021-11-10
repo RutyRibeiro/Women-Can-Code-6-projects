99e86438facb4a3be2081b5225a37dbe9dd701a5
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-10 20:49:00 -0300

    examples of the second class WCC

---
+++ day 3/example.js
@@ -0,0 +1,15 @@
+const name = 'Ruty Ribeiro Pena';
+const age = 21;
+const majority = true;
+
+console.log(name);
+console.log(age)
+console.log(majority)
+
+// ao imprimir da seguinte form é interessante observar que ao imprimir há espaço entre as variaveis
+console.log(name,age,majority)
+// dessa forma as variaveis são concatenadas
+console.log(name+age+majority)
+
+
+console.log("My name is "+name+" and I'm "+age+" years old")
