918fb9c252bfbcd12d6e5e8d84c7ab9bd3a0a57e
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-10 20:46:13 -0300

    ex01 - first part

---
+++ day 3/ex01.js
@@ -0,0 +1,16 @@
+/*
+crie 4 variaveis para armazenar os seguintes dados referentes a um carro: 
+ - modelo
+ - cor
+ - ano 
+ - novo(Verdadeiro ou falso)
+*/
+const modelo = "Astra";
+const cor = "prata";
+const ano = 2004;
+const novo = true;
+
+console.log(modelo);
+console.log(cor);
+console.log(ano);
+console.log(`carro novo? ${novo ? 'sim':'nao'}`);
