dc187d7b58cbe4973c4a477ac187fe7fa3453c5b
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-11 13:20:17 -0300

    ex01 - second part

---
+++ day 3/pt2/ex01.js
@@ -0,0 +1,17 @@
+/*
+    Crie uma variável para armazenar o resultado de cada das operações e imprima o resultado delas.
+        1. Soma entre 3 + 7
+        2. Subtração entre 135 e 29
+        3. Multiplicação entre 4 e 8
+        4. Divisão entre 225 e 5
+*/
+const sum = 3+7;
+const sub = 135-29;
+const multi = 4*8;
+const divi = 225/5;
+
+// impressão dos resultados
+console.log(`3 + 7 = ${sum}`);
+console.log(`135 - 29 = ${sub}`);
+console.log(`4 x 8 = ${multi}`);
+console.log(`225 / 5 = ${divi}`);
