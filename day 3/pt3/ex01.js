744407113aa41c14cb51058675dbf655333b1ee5
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-11 13:21:21 -0300

    ex01 - third part

---
+++ day 3/pt3/ex01.js
@@ -0,0 +1,16 @@
+/*
+    Crie uma variável chamada `hojeVaiChover` e uma condicional `if` que imprima na tela o seguinte:
+
+        - "Leve seu guarda-chuva, hoje vai chover" se o valor for verdadeiro
+        - "Fique tranquilo, hoje será um dia se sol" se o valor for falso
+*/
+
+const hojeVaiChover = true;
+
+// if (hojeVaiChover) {
+//     console.log("Leve seu guarda-chuva, hoje vai chover")
+// }else{
+//     console.log("Fique tranquilo, hoje será um dia se sol")
+// }
+
+console.log(hojeVaiChover ? "Leve seu guarda-chuva, hoje vai chover":"Fique tranquilo, hoje será um dia se sol")
