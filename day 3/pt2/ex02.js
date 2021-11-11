11e4822cf2d225bc5a661e6873338cab91f9a0dc
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-11 13:20:27 -0300

    ex02 - second part

---
+++ day 3/pt2/ex02.js
@@ -0,0 +1,20 @@
+// Vamos explorar atribuições composta em operações matemáticas! Imprima o valor de cada uma das operações a seguir:
+
+const saldo = 50;
+
+const saldoComBonus = saldo + 20;
+
+const saldoComDesconto = saldoComBonus / 0.5;
+
+const saldoFinal = saldoComDesconto;
+
+
+// Impessão na tela
+console.log(`saldoInicial = ${saldo}`)
+
+console.log(`saldoComBonus = ${saldoComBonus}`)
+
+console.log(`saldoComDesconto = ${saldoComDesconto}`)
+
+console.log(`saldoFinal = ${saldoComDesconto}`)
+
