82122cea84d2165671c693df96b46d0e92f83ff0
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-11 13:23:24 -0300

    geral annotations

---
+++ annotations.js
@@ -0,0 +1,53 @@
+/*
+CASE SENSITIVE
+
+as variaveis: 
+var nome = 'Ruty'
+var Nome = 'Ruty'
+var NOME = 'Ruty'
+são diferentes por este motivo 
+
+---------------------------------------------
+
+DECLARAÇÃO DE VARIÁVEIS
+
+var: global
+let: local
+const = o valor não pode ser modificada
+
+---------------------------------------------
+
+TIPOS DE VARIÁVEIS
+
+string     "89737912hjksdhajsd##$%"
+number     29 
+boolean    true/false
+object     {} 
+undefined
+symbol
+
+---------------------------------------------
+
+OPERADORES
+
++ somas
+- subtrações
+* multiplicações
+/ divisoes
+% resto
+
+---------------------------------------------
+
+COMPARAÇÃO
+
+==  tem o mesmo valor
+=== tem o mesmo valor e são do mesmo tipo
+!=  valor diferente
+!== valor e tipo diferentes
+>   maior
+<   menor
+>=  maior ou igual
+<=  menor ou igual
+
+---------------------------------------------
+*/
