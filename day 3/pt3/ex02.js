fb96e16a516aeb04ff62ac913f465db300bcfc56
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-11 13:21:36 -0300

    ex02 - third part

---
+++ day 3/pt3/ex02.js
@@ -0,0 +1,24 @@
+//Crie variáveis para cada uma das comparações e imprima o resultado delas
+
+const maior = 5 > 1
+const menor = 2000000 < 3000000000
+const comparacao = "Banana" == "Banana"
+const comparacaoEstrita = "50" === 50
+const diferente = "Carro" != "bicicleta"
+const maiorNovamente = 50 > 27
+const menorIgual = 975 <= 1200
+
+
+// impressão dos resultados
+console.log(`5 é maior que 1 = ${maior}`)
+console.log(`2000000 é menor que 3000000000 = ${menor}`)
+console.log(`"Banana" é igual a "Banana" = ${comparacao}`)
+console.log(`"50" é estritamente igual a 50 = ${comparacaoEstrita}`)
+console.log(`"Carro" é diferente de "bicicleta" = ${diferente}`)
+console.log(`50 é maior que 27 = ${maiorNovamente}`)
+console.log(`975 é menor ou igual a 1200 = ${menorIgual}`)
+
+
+
+
+
