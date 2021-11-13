37be1e8b0c023522db240e38784d103ab799c525
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-12 22:05:59 -0300

    examples of fifth day class

---
+++ day5/example.js
@@ -0,0 +1,18 @@
+function nameOfTheFirstFunction(){
+    console.log("this works")
+}
+
+nameOfTheFirstFunction();
+
+function functionWithParam(myParam){
+    console.log(myParam)
+}
+
+functionWithParam("param")
+
+function valorReturn(){
+
+    return "this is a return"
+}
+
+console.log(valorReturn())
