b32233a76e42e689e09c2c029a13209399c0dc9e
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-11-12 22:12:57 -0300

    update to exercise specifications

--- day5/pt1/ex04.js
+++ day5/pt1/ex04.js
@@ -1,4 +1,15 @@
+/*
+    Esse trecho irá chamar a função `calculaSoma`, que retorna um valor de `17` e o atribui à variável `resultadoSoma`.
 
+            let resultado = 0;
+
+            function processaParametros(numero) {
+                return (numero + 3) / 5;
+            }
+
+    Chame a função processaParametros com um argumento 7 e atribua seu valor de retorno à variável resultado;
+
+ */
 
 
 let resultado = 0;
--- day5/pt1/ex05.js
+++ day5/pt1/ex05.js
@@ -1,4 +1,13 @@
+/*
+    Vamos construir uma função validaMaioridade para validar se o ano de nascimento fornecido corresponde a uma pessoa com mais de 18 e assim seja liberada ou não a entrada dela em um determinado show.
 
+    1. Receba um parâmetro numérico `anoNascimento` ;
+    2. Valide se, pela data de nascimento, se trata de alguém com maior de 18 anos;
+    3. Se for maior que 18 anos, retorne uma `string` dizendo `"Liberação autorizada"`;
+    4. Se for igual a 18 anos, retorne uma `string` dizendo `"Validar documento de identidade"`;
+    5. Se for menor de 18 anos, retorne uma `string` dizendo `"ATENÇÃO: Liberação NÃO autorizada"`;
+    6. Atribua o retorno a uma variável chamada `resultado` e imprima-a.
+*/
 var data = new Date();
 
 const validamaioridade = (anoDeNascimento) => {
